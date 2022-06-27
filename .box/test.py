333d8184083b7fd3bb64f397c03792e2d0e79a5e
Anselm Kiefner <github@anselm.kiefner.de>
2022-06-27 11:00:38 +0200

    test2

---
+++ .box/test.py
@@ -0,0 +1,29 @@
+def foobar():
+    """
+    foobar!
+    
+    ```mermaid
+classDiagram
+    Animal <|-- Duck
+    Animal <|-- Fish
+    Animal <|-- Zebra
+    Animal : +int age
+    Animal : +String gender
+    Animal: +isMammal()
+    Animal: +mate()
+    class Duck{
+        +String beakColor
+        +swim()
+        +quack()
+    }
+    class Fish{
+        -int sizeInFeet
+        -canEat()
+    }
+    class Zebra{
+        +bool is_wild
+        +run()
+    }
+```
+    """
+    pass
